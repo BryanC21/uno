2dd9cd711871dbe9dbe14eceedfbead270a8c37e
Bryan Caldera <bryanicaldera@gmail.com>
2020-09-07 22:09:10 -0700

    Fixed errors

--- src/GameTypes/GameNoUI.java
+++ src/GameTypes/GameNoUI.java
@@ -1,5 +1,7 @@
 package GameTypes;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import CardHolders.Deck;
 import CardHolders.Hand;
 import Cards.*;
@@ -10,11 +12,13 @@ import Cards.*;
  * 	Take care of player hands
  * 	Control computers
  * Player 1 is user, player 2 is comp1 and so on clockwise for 4 total players
+ * Game Ends if no input for 60 seconds of user turn
  * @author Bryan Caldera
  */
 public class GameNoUI {
 	
-	Scanner input;
+	//input;
+	BufferedReader in2;
 	/**Hand of each player*/
 	private Hand user, comp1, comp2, comp3;
 	/**The deck we draw from (Not infinite)*/
@@ -36,23 +40,28 @@ public class GameNoUI {
 		comp2 = new Hand("Computer 2");
 		comp3 = new Hand("Computer 3");
 		user = new Hand("The user");
-		input = new Scanner(System.in);
+		//input = new Scanner(System.in);
+		in2 = new BufferedReader(new InputStreamReader(System.in));
 		
 		setupGame();
 		runGame();
 	}
-	
+
 	/**This will print if someone wins. It will get the game started with user playing first.*/
 	private void runGame()
 	{
-		userTurn();
+		try {
+			userTurn();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		System.out.println(winner + " has won");
+		System.out.println("END");
 	}
 	
 	/**Lets the user go through their turn and checks if they win.
 	 * At beginning of turn checks if player was affected by any special cards*/
-	private void userTurn()
-	{
+	private void userTurn() throws IOException {
 		if (centerCard instanceof DrawFour && !centerCard.getUsed()) {
 			 user.addToHand(deck1.drawCard());
 			 user.addToHand(deck1.drawCard());
@@ -67,9 +76,13 @@ public class GameNoUI {
 			centerCard.setUsed();
 			System.out.println("Next player drew 2 cards");
 		}
-		
-		int choice = askForInput(); //The index of the card the user picks
-		
+		int choice = 0; //The index of the card the user picks
+		try {
+			choice = askForInput();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		if (choice == -1) { //User wants to draw a card
 			user.addToHand(deck1.drawCard());
 			System.out.println("You drew a card!");
@@ -78,6 +91,11 @@ public class GameNoUI {
 			
 		} else if (choice == -2) { //User wants to quit
 			winner = "No one";
+			try {
+				in2.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 			System.out.println("You quit!");
 			return;
 			
@@ -95,7 +113,22 @@ public class GameNoUI {
 				if (centerCard instanceof WildCard || centerCard instanceof DrawFour) {
 					System.out.println("Pick a color from: Yellow, Green, Blue, Red");
 					String newColor = "Yellow";
-					newColor = input.next();
+					//newColor = input.next();
+					long startedInput = System.currentTimeMillis();
+					while((System.currentTimeMillis()-startedInput) < 60*1000 && !in2.ready()) {
+						try {
+							Thread.sleep(500);/*loops 60 seconds*/
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+					if (in2.ready()) {
+						newColor = in2.readLine();
+					} else {
+						System.out.println("Game Ended - Idle");
+						in2.close();
+						System.exit(0);
+					}
 					
 					while (!validColor) { // Makes sure color input is valid
 						if (newColor.equalsIgnoreCase("Red") || newColor.equalsIgnoreCase("Green") 
@@ -104,7 +137,21 @@ public class GameNoUI {
 							break;
 						}
 						System.out.println(newColor + " is not a valid color!\nInput a new color:");
-						newColor = input.next();
+						startedInput = System.currentTimeMillis();
+						while((System.currentTimeMillis()-startedInput) < 60*1000 && !in2.ready()) {
+							try {
+								Thread.sleep(500);
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+							/*loops 60 seconds*/}
+						if (in2.ready()) {
+							newColor = in2.readLine();
+						} else {
+							System.out.println("Game Ended - Idle");
+							in2.close();
+							System.exit(0);
+						}
 					}
 					
 					System.out.println("The color is now: " + newColor);
@@ -129,6 +176,11 @@ public class GameNoUI {
 				}
 				else
 				{
+					try {
+						in2.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
 					winner = "The user";
 				}
 			
@@ -141,10 +193,12 @@ public class GameNoUI {
 	private boolean validInput(Hand player, int choice)
 	{
 		//Conditions:
-		boolean sameNum = player.getCard(choice).getNumber() == centerCard.getNumber();
-		boolean sameColor = player.getCard(choice).getColor().equalsIgnoreCase(centerCard.getColor());
+		boolean sameNum = (player.getCard(choice).getNumber() == centerCard.getNumber()) && (centerCard.getNumber() != 999);
+		boolean sameColor = (player.getCard(choice).getColor().equalsIgnoreCase(centerCard.getColor())) && !(centerCard.getColor().equals("none")) ;
 		boolean wild = player.getCard(choice) instanceof WildCard || player.getCard(choice) instanceof DrawFour;
-		if(sameNum || sameColor || wild)
+		//DrawTwo or Skip or Reverse - Different Color
+		boolean sameType = (player.getCard(choice) instanceof DrawTwo && centerCard instanceof DrawTwo) || (player.getCard(choice) instanceof Skip && centerCard instanceof Skip) || (player.getCard(choice) instanceof Reverse && centerCard instanceof Reverse);
+		if(sameNum || sameColor || wild || sameType)
 		{
 			return true;
 		}
@@ -154,15 +208,30 @@ public class GameNoUI {
 	}
 	
 	/**Asks the user to pick a card from their hand, draw a card, or quit the game.*/
-	private int askForInput()
-	{
+	private int askForInput() throws IOException {
 		System.out.println("\nIt is your turn\n" + user);
-		System.out.println("The center card is: " + centerCard);
+		System.out.println("***\nThe center card is: " + centerCard+"\n***");
 		if (centerCard instanceof WildCard || centerCard instanceof DrawFour) {
 			System.out.println("The color is " + centerCard.getColor());
 		}
 		System.out.println("Which card will you play?\nType number next to the card to play that card.\nType \"-1\" to draw a card.\nOr type \"-2\" to quit the game");
-		int index = input.nextInt();
+		long startedInput = System.currentTimeMillis();
+		int index = 0;
+		while((System.currentTimeMillis()-startedInput) < 60*1000 && !in2.ready()) {
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			/*loops 60 seconds*/}
+		if (in2.ready()) {
+			index = Integer.parseInt(in2.readLine());
+		} else {
+			System.out.println("Game Ended - Idle");
+			in2.close();
+			System.exit(0);
+		}
+		System.out.println("Input: "+index);
 		if(index <= user.getSize() && index > 0)
 		{
 			return index - 1;
@@ -188,13 +257,13 @@ public class GameNoUI {
 			compPlaying = "Computer 1";
 			ai(comp1);
 			hasWon = checkForWinner(comp1);
-		}
+		} else
 		if(upNext == 3)
 		{
 			compPlaying = "Computer 2";
 			ai(comp2);
 			hasWon = checkForWinner(comp2);
-		}
+		} else
 		if(upNext == 4)
 		{
 			compPlaying = "Computer 3";
@@ -207,7 +276,11 @@ public class GameNoUI {
 			upNext = checkNextPlayer();
 			if(upNext == 1)
 			{
-				userTurn();
+				try {
+					userTurn();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 			else
 			{
@@ -216,6 +289,11 @@ public class GameNoUI {
 		}
 		else
 		{
+			try {
+				in2.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 			winner = compPlaying;
 		}
 		
@@ -292,7 +370,7 @@ public class GameNoUI {
 		for(int i = 0; i < player.getSize(); i++)
 		{
 			if((player.getCard(i).getColor().equalsIgnoreCase(centerCard.getColor())
-					&& player.getCard(i).getNumber() != 999) || (player.getCard(i).getNumber() == 
+					&& !player.getCard(i).getColor().equals("none")) || (player.getCard(i).getNumber() ==
 					centerCard.getNumber() && player.getCard(i).getNumber() != 999))
 			{
 				//Pick the card with the biggest number
@@ -355,7 +433,7 @@ public class GameNoUI {
 			player.removeFromHand(bestIndex);
 			System.out.println(player.getName() + " played " + centerCard);
 			if (centerCard instanceof Skip && !centerCard.getUsed()) {
-				upNext++;
+				upNext = checkNextPlayer();
 				centerCard.setUsed();
 			}
 			if (centerCard instanceof Reverse && !centerCard.getUsed()) {
